ca870289ccc2745340f8ad3da20ef4b79277bd91
Vinicius Crispim <vinicrispim02@hotmail.com>
2023-07-12 15:52:19 -0300

    conectando API viaCEP

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,4 @@
-alert("AAAAI MESSI");
+var consultaCEP = fetch("https://viacep.com.br/ws/83708100/json/").then(response => response.json()).
+then(responseBody => console.log(responseBody.cep));
+
+console.log(consultaCEP);
