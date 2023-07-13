e70ab67960dfcf6ee65c96ac70683e9848ba4eb8
Vinicius Crispim <vinicrispim02@hotmail.com>
2023-07-13 19:21:15 -0300

    Complementa endereço

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const endereco = {
+    'logradouro': '',
+    'bairro': '',
+    'localidade': '',
+    'uf': '',
+    'cep': '',
+};
+
+function searchCEP(valor) {
+    var cep = valor.replace(/\D/g, '');
+
+    if (cep != "") {
+        if (/^[0-9]{8}$/.test(cep)) {
+            fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => response.json()).
+            then(responseBody => {
+                addAddress(responseBody);
+            });
+        }
+    }
+
+
+    function addAddress(responseBody) {
+        endereco.logradouro = responseBody.logradouro;
+        endereco.bairro = responseBody.bairro;
+        endereco.localidade = responseBody.localidade;
+        endereco.uf = responseBody.uf;
+        endereco.cep = responseBody.cep;
+        showAddress(endereco);
+    }
+
+    function showAddress(endereco) {
+        document.querySelector("#endereco").value = endereco.logradouro;
+        document.querySelector("#bairro").value = endereco.bairro;
+        document.querySelector("#estado").value = endereco.uf;
+        document.querySelector("#cidade").value = endereco.localidade;
+    }
+}
--- assets/script.js
+++
@@ -1,4 +0,0 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/83708100/json/").then(response => response.json()).
-then(responseBody => console.log(responseBody.cep));
-
-console.log(consultaCEP);
